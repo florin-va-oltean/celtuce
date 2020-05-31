3fd186a2017f40fc1b0682b9810321b94fbd2847
florin.oltean <florin.oltean@qsrlab.com>
2020-05-31 16:16:13 +0300

    included Quasar private repository for internal builds until / if pull request is approved

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
                  [celtuce-core ~celtuce-version]
                  [celtuce-pool ~celtuce-version]
                  [celtuce-manifold ~celtuce-version]]
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :repositories [["releases" {
+                              :url "https://repo.deps.co/quasar-software-research/releases"
+                              :sign-releases false
+                              :username :env/deps_key
+                              :password :env/deps_secret}]
+                 ["snapshots" {
+                               :url "https://repo.deps.co/quasar-software-research/snapshots"
+                               :sign-releases false
+                               :username :env/deps_key
+                               :password :env/deps_secret}]])
